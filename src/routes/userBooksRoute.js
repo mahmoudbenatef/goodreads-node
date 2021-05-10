9f2946327616ffb4d1276c151e3b57ce1206ea06
Abobakr <abobakryousre@gmail.com>
2021-05-10 05:56:23 +0200

    feat(routing-design): craeted userBooks router and controller

---
+++ src/routes/userBooksRoute.js
@@ -0,0 +1,26 @@
+const express = require("express");
+// mergeParams to receved the parent params in the request object
+const Router = express.Router({ mergeParams: true });
+const userBooksController = require("../controllers/userBooksController");
+
+Router.get("/", (req, res) => {
+  userBooksController.getAllBooks(req, res);
+});
+
+Router.get("/:id", (req, res) => {
+  userBooksController.getBookById(req, res);
+});
+
+Router.post("/", (req, res) => {
+  userBooksController.addNewBook(req, res);
+});
+
+Router.patch("/:id", (req, res) => {
+  userBooksController.changeBookShelf(req, res);
+});
+
+Router.delete("/:id", (req, res) => {
+  userBooksController.deleteBook(req, res);
+});
+
+module.exports = Router;
