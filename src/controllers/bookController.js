8678861d77fe600750a50264830beb46a7fda4e7
Abobakr <abobakryousre@gmail.com>
2021-05-10 05:24:36 +0200

    feat(routing-design): craeted book router and controller

---
+++ src/controllers/bookController.js
@@ -0,0 +1,15 @@
+const getAllBooks = (req, res) => {};
+const getBookById = (req, res) => {};
+const createBook = (req, res) => {};
+const editBook = (req, res) => {};
+const deleteBook = (req, res) => {};
+const updateBook = (req, res) => {};
+
+module.exports = {
+  getAllBooks,
+  getBookById,
+  createBook,
+  editBook,
+  deleteBook,
+  updateBook,
+};
