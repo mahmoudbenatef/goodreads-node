3fa1782cf146c177bedefecfe3a4c16e249410d0
Abobakr <abobakryousre@gmail.com>
2021-05-11 22:29:04 +0200

    feat(routing-design): add success codes To  statusCode

--- src/helper/statusCode.js
+++ src/helper/statusCode.js
@@ -1,4 +1,7 @@
 module.exports = {
+  Success: 200,
+  Created: 201,
+  NoContent: 204,
   BadRequest: 400, // use it when client-side input fails validation
   UnAuthorized: 401, //  use it when user is unauthenticated
   Forbidden: 403, //  use it when user is authenticated, but it’s not allowed to access a resource
