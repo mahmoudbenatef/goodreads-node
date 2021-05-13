1596ffc4dfb3579c2bfaaf7da79791eda5c70a18
Aya <ayahamedabdulaziz@gmail.com>
2021-05-13 19:04:42 +0200

    hotfix(shelve-book): Refactoring rate and shelve book methods..

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -5,161 +5,151 @@ const handler = require("../helper/controllersHelper");
 const UserBookModel = require("../models/userBookModel");
 const shelves = require("../helper/shelves");
 
-const getAllBooks = async (req, res) => { // get all books
-    const allBooks = await bookModel.find({});
-    if (allBooks.length > 0) 
-        return res.status(statusCode.Success).json(allBooks);
-     // collection has data
-    return res.status(statusCode.NoContent).end(); // collection is empty
+const getAllBooks = async (req, res) => {
+  const allBooks = await bookModel.find({});
+  if (allBooks.length > 0) return res.status(statusCode.Success).json(allBooks);
+  return res.status(statusCode.NoContent).end();
 };
-
 
 const getBookById = async (req, res, next) => {
-    const bookId = req.params.id;
-    // gard
-    if (! bookId) 
-        handler.handelEmptyData(res);
-    try { // get the book and return it
-        const book = await bookModel.findById(bookId);
-        if (book) 
-            return res.status(statusCode.Success).json(book);
-        return res.status(statusCode.NoContent).end();
-    } catch (error) { // maybe there is no book with the given id
-        next(error);
-    }
+  const bookId = req.params.id;
+  if (!bookId) handler.handelEmptyData(res);
+  try {
+    const book = await bookModel.findById(bookId);
+    if (book) return res.status(statusCode.Success).json(book);
+    return res.status(statusCode.NoContent).end();
+  } catch (error) {
+    next(error);
+  }
 };
-
 
 const createBook = (req, res, next) => {
-    const data = req.body;
-    // function gard
-    if (! data) 
-        handler.handelEmptyData(res);
-    // validate the data before create a new book
-    bookValidator.validateData(data, (err) => {
-        if (err) 
-            return next(err);
-        try { // craete a new book
-            const newBook = bookModel.create({
-                ... data
-            });
-            res.status(statusCode.Created).end();
-        } catch (error) {
-            next(error);
-        }
-    });
+  const data = req.body;
+  if (!data) handler.handelEmptyData(res);
+  bookValidator.validateData(data, (err) => {
+    if (err) return next(err);
+    try {
+      const newBook = bookModel.create({
+        ...data,
+      });
+      res.status(statusCode.Created).end();
+    } catch (error) {
+      next(error);
+    }
+  });
 };
-
 
 const updateBook = (req, res) => {
-    const data = req.body;
-    // function gard
-    if (! data) 
-        handler.handelEmptyData(res);
-    // validate the data before update it
-    bookValidator.validateData(data, async (err) => {
-        if (err) 
-            return next(err);
-        try { // update the book with new data
-            const updatedBook = await bookModel.findByIdAndUpdate({
-                _id: req.params.id
-            }, {
-                ... data
-            });
-            res.status(statusCode.NoContent).end();
-        } catch (error) {
-            next(error);
-        }
-    });
+  const data = req.body;
+  if (!data) handler.handelEmptyData(res);
+  bookValidator.validateData(data, async (err) => {
+    if (err) return next(err);
+    try {
+      const updatedBook = await bookModel.findByIdAndUpdate(
+        { _id: req.params.id, },{ ...data,}
+      );
+      res.status(statusCode.NoContent).end();
+    } catch (error) {
+      next(error);
+    }
+  });
 };
-
 
 const deleteBook = async (req, res, next) => {
-    const bookId = req.params.id;
-    if (! bookId) 
-        handler.handelEmptyData(res);
-    try {
-        const resulat = await bookModel.findByIdAndDelete({_id: bookId});
-        res.status(statusCode.NoContent).end();
-    } catch (error) {
-        next(error);
-    }
+  const bookId = req.params.id;
+  if (!bookId) handler.handelEmptyData(res);
+  try {
+    const resulat = await bookModel.findByIdAndDelete({ _id: bookId });
+    res.status(statusCode.NoContent).end();
+  } catch (error) {
+    next(error);
+  }
 };
 
-// Update the average rating of the given book
 async function updateBookAvgRating(bookId) {
-    const ratings = await UserBookModel.find({
-        book: bookId,
-        rating: {
-            $exists: true
-        }
-    });
-    const avgRating = ratings.reduce((total, next) => total + next.rating, 0) / ratings.length;
-    const book = await bookModel.findOneAndUpdate({
-        _id: bookId
-    }, {
-        avgRating: avgRating
-    }, {useFindAndModify: false});
+  const ratings = await UserBookModel.find({
+    book: bookId, rating: { $exists: true, },
+  });
+  const avgRating =
+    ratings.reduce((total, next) => total + next.rating, 0) / ratings.length;
+  const book = await bookModel.findOneAndUpdate(
+    { _id: bookId },
+    { avgRating: avgRating },
+    { useFindAndModify: false }
+  );
+}
+
+async function updateExistingRating({ userId, bookId, rating }) {
+  return await UserBookModel.findOneAndUpdate(
+    { book: bookId, user: userId },
+    { rating: rating },
+    { useFindAndModify: false }
+  );
+}
+
+async function addNewRating({ bookId, userId, rating }) {
+  const userBookInstance = new UserBookModel({
+    book: bookId,
+    user: userId,
+    rating: rating,
+    shelf: shelves.READ,
+  });
+  return await userBookInstance.save();
 }
 
 // Rate book.
 const rateBook = async (request, response) => {
   try {
-    const userId = request.body.user;
-    const bookId = request.params.id;
-    const rating = request.body.rating;
-    const book = await UserBookModel.findOneAndUpdate({
-        book: bookId,
-        user: userId
-    }, {
-        rating: rating
-    }, {useFindAndModify: false});
-    if (book) 
-        response.sendStatus(statusCode.Success);
-      else {
-        const userBookInstance = new UserBookModel({book: bookId, user: userId, rating: rating, shelf: shelves.READ});
-        const userBookDoc = await userBookInstance.save();
-        response.status(statusCode.Success).json(userBookDoc);
-    } updateBookAvgRating(bookId);
+    const rating = {
+      userId: request.body.user,
+      bookId: request.params.id,
+      rating: request.body.rating,
+    };
+    const book = await updateExistingRating(rating);
+    if (book) response.sendStatus(statusCode.Success);
+    else {
+      const userBookDoc = addNewRating(rating);
+      response.status(statusCode.Success).json(userBookDoc);
+    }
+    updateBookAvgRating(rating.bookId);
   } catch (err) {
     return response.status(statusCode.ServerError).json(err);
   }
 };
 
-async function removeBookFromShelf(bookId,userId) {
-  await UserBookModel.findOneAndDelete({book: bookId, user: userId});
+async function removeBookFromShelf({ bookId, userId }) {
+  await UserBookModel.findOneAndDelete({ book: bookId, user: userId });
 }
 
-async function addBookToShelf(bookId,userId,shelf) {
-  const userBookInstance = new UserBookModel({book: bookId, user: userId, shelf: shelf});
+async function addBookToShelf({ bookId, userId, shelf }) {
+  const userBookInstance = new UserBookModel({
+    book: bookId,
+    user: userId,
+    shelf: shelf,
+  });
   await userBookInstance.save();
 }
 
-async function updateBookShelf(bookId,userId,shelf) {
-  return await UserBookModel.findOneAndUpdate({
-    book: bookId,
-    user: userId
-  }, {
-    shelf: shelf
-  }, {useFindAndModify: false});
+async function updateBookShelf({ bookId, userId, shelf }) {
+  return await UserBookModel.findOneAndUpdate(
+    { book: bookId, user: userId },
+    { shelf: shelf },
+    { useFindAndModify: false }
+  );
 }
 
-// Rate book.
 const shelveBook = async (request, response) => {
   try {
-    const userId = request.body.user;
-    const bookId = request.params.id;
-    const shelf = request.body.shelf;
-    if (shelf === 0) 
-      removeBookFromShelf(bookId,userId);
-    else 
-    {
-      const book = await updateBookShelf(bookId,userId,shelf)
-      console.log(book);
-      if (book) 
-        response.sendStatus(statusCode.Success);
-      else
-        addBookToShelf(bookId,userId,shelf);
+    const bookShelf = {
+      userId: request.body.user,
+      bookId: request.params.id,
+      shelf: request.body.shelf,
+    };
+    if (bookShelf.shelf === 0) removeBookFromShelf(bookShelf);
+    else {
+      const book = await updateBookShelf(bookShelf);
+      if (book) response.sendStatus(statusCode.Success);
+      else addBookToShelf(bookShelf);
     }
     response.status(statusCode.Success);
   } catch (err) {
@@ -167,13 +157,12 @@ const shelveBook = async (request, response) => {
   }
 };
 
-
 module.exports = {
-    getAllBooks,
-    getBookById,
-    createBook,
-    deleteBook,
-    updateBook,
-    rateBook,
-    shelveBook
+  getAllBooks,
+  getBookById,
+  createBook,
+  deleteBook,
+  updateBook,
+  rateBook,
+  shelveBook,
 };
