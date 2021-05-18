85e78b89d0e2ef103d0a9e48eb9092e88ce18bae
Ahmed <amamdouh247@gmail.com>
2021-05-18 12:33:39 +0200

    feat: list all categories and books of each category

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,7 +4,9 @@ const bcrypt = require('bcrypt'),
 jwt = require('jsonwebtoken')
 
 var mongoose = require('mongoose')
-const {loginRequired} = require("../middlewares/AuthMiddleware.js")
+const {loginRequired} = require("../middlewares/AuthMiddleware.js");
+const CategoryModel = require("../models/categoryModel");
+const BookModel = require("../models/bookModel");
 
 
 const register = async function(req, res) {
@@ -55,8 +57,27 @@ const users= async function (req,res){
       
 }
 
+const getCategories =  async (req, res) => {
+    const categories = await CategoryModel.find({});
+    if (categories) {
+        return res.status(200).json(categories);   
+    }
+    
+    return res.status(500).end();
+};
+
+
+const getCategoryBooks =  async (req, res) => {
+    const categoryId = req.params.id;
+    const books = await BookModel.find({category: categoryId});
+    if (books) {
+        return res.status(200).json(books);   
+    }
+    
+    return res.status(500).end();
+};
 
 
 module.exports = {
-    register, sign_in, users
+    register, sign_in, users, getCategories, getCategoryBooks
 }
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -4,7 +4,7 @@ const express = require("express"),
     jwt = require('jsonwebtoken'),
     bcrypt = require('bcrypt')
 
-const {register, sign_in,users } = require('../controllers/userController.js');
+const {register, sign_in,users, getCategories, getCategoryBooks } = require('../controllers/userController.js');
 const {loginRequired} = require("../middlewares/AuthMiddleware.js")
 const {upload}= require("../middlewares/MulterMiddleware.js")
 const userBooksRouter = require("./userBooksRoute");
@@ -15,5 +15,10 @@ Router.post("/signin", sign_in)
 
 Router.get("/", loginRequired, users)
 Router.use("/:userId/books", userBooksRouter);
+
+//categories
+Router.get("/categories", getCategories);
+
+Router.get("/categories/:id", getCategoryBooks);
 
     module.exports = Router
